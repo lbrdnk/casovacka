9e3a8de06d3d88327ad24a0408ab14309cb917d3
Libor Danek <lbr.dnk@posteo.net>
2023-01-05 13:58:02 +0100

    Add shadow-cljs

---
+++ src/main/casovacka/core.cljs
@@ -0,0 +1,30 @@
+(ns casovacka.core
+  (:require
+   ["react-native" :as rn]
+   [reagent.core :as r]
+   [shadow.expo :as expo]))
+
+(def styles
+  ^js (-> {:container
+           {:flex 1
+            :backgroundColor "#fff"
+            :alignItems "center"
+            :justifyContent "center"}
+           :title
+           {:fontWeight "bold"
+            :fontSize 24
+            :color "blue"}}
+          (clj->js)
+          (rn/StyleSheet.create)))
+
+(defn root []
+  [:> rn/View {:style (.-container styles)}
+   [:> rn/Text {:style (.-title styles)} "Hello friend!"]])
+
+(defn start
+  {:dev/after-load true}
+  []
+  (expo/render-root (r/as-element [root])))
+
+(defn init []
+  (start))
