b08db66839e4af1613e96f4535b04771c9f3391d
Libor Danek <lbr.dnk@posteo.net>
2023-01-08 20:40:42 +0100

    [WIP] Working example with font
    https://shopify.github.io/react-native-skia/docs/text/text
    
    https://fonts.google.com/specimen/Roboto

--- App.js
+++ App.js
@@ -4,23 +4,54 @@
 // export default App;
 
 
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+// import { Canvas, Circle, Group, Text } from "@shopify/react-native-skia";
 
-export default App = () => {
-  const size = 256;
-  const r = size * 0.33;
+// export default App = () => {
+//   const fontSize = 32;
+//   const size = 256;
+//   const r = size * 0.33;
+//   return (
+//     <Canvas style={{ flex: 1 }}>
+//       <Group blendMode="multiply">
+//         <Circle cx={r} cy={r} r={r} color="cyan" />
+//         <Circle cx={size - r} cy={r} r={r} color="magenta" />
+//         <Circle
+//           cx={size / 2}
+//           cy={size - r}
+//           r={r}
+//           color="yellow"
+//         />
+//       </Group>
+//       {/* <Fill color="white" /> */}
+//       <Text
+//         x={0}
+//         y={fontSize}
+//         text="Hello World"
+//         // font={font}
+//       />
+//     </Canvas>
+//   );
+// };
+
+import {Canvas, Text, useFont, Fill} from "@shopify/react-native-skia";
+ 
+export const App = () => {
+  const fontSize = 32;
+  const font = useFont(require("./Roboto-Black.ttf"), fontSize);
+  if (font === null) {
+    return null;
+  }
   return (
     <Canvas style={{ flex: 1 }}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={size - r} cy={r} r={r} color="magenta" />
-        <Circle
-          cx={size / 2}
-          cy={size - r}
-          r={r}
-          color="yellow"
-        />
-      </Group>
+      <Fill color="white" />
+      <Text
+        x={0}
+        y={fontSize}
+        text="Hello World"
+        font={font}
+      />
     </Canvas>
   );
 };
+
+export default App
