c448816f69a76d4ae3978c2b4b37582a4cd12558
Libor Danek <lbr.dnk@posteo.net>
2023-01-08 20:53:33 +0100

    [WIP] Animation example
    https://shopify.github.io/react-native-skia/docs/animations/animations

--- App.js
+++ App.js
@@ -33,25 +33,42 @@
 //   );
 // };
 
-import {Canvas, Text, useFont, Fill} from "@shopify/react-native-skia";
+// import {Canvas, Text, useFont, Fill} from "@shopify/react-native-skia";
  
-export const App = () => {
-  const fontSize = 32;
-  const font = useFont(require("./Roboto-Black.ttf"), fontSize);
-  if (font === null) {
-    return null;
-  }
+// export const App = () => {
+//   const fontSize = 32;
+//   const font = useFont(require("./Roboto-Black.ttf"), fontSize);
+//   if (font === null) {
+//     return null;
+//   }
+//   return (
+//     <Canvas style={{ flex: 1 }}>
+//       <Fill color="white" />
+//       <Text
+//         x={0}
+//         y={fontSize}
+//         text="Hello World"
+//         font={font}
+//       />
+//     </Canvas>
+//   );
+// };
+
+// export default App
+
+import React, { useState } from "react";
+import { Canvas, Rect, useSpring } from "@shopify/react-native-skia";
+import { Button, StyleSheet } from "react-native";
+ 
+export default function AnimationExample() {
+  const [toggled, setToggled] = useState(false);
+  const position = useSpring(toggled ? 100 : 0);
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Fill color="white" />
-      <Text
-        x={0}
-        y={fontSize}
-        text="Hello World"
-        font={font}
-      />
-    </Canvas>
+    <>
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={position} y={100} width={10} height={10} color={"red"} />
+      </Canvas>
+      <Button title="Toggle" onPress={() => setToggled((p) => !p)} />
+    </>
   );
 };
-
-export default App
