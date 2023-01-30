24b51cf1a25234106386561831c187780c7b727e
Libor Danek <lbr.dnk@posteo.net>
2023-01-30 15:17:36 +0100

    [WIP] Add navigation stub screens

--- src/main/casovacka/navigation.cljs
+++ src/main/casovacka/navigation.cljs
@@ -10,15 +10,25 @@
 (defonce Stack (createNativeStackNavigator))
 
 (defn Home [props]
-  #_[:> rn/View {:align-items :center
-               :flex 1 
-               :justify-content :center}
-   [:> rn/Text "home"]]
   [:> rn/View {:style {:flex 1 :justify-content "center" :align-items "center"}}
-   [:> rn/Text "home"]])
+   [:> rn/Text "home"]
+   [:> rn/Button {:on-press #(.navigate (:navigation props) "interval") 
+                  :title "go to interval"}]])
+
+(defn Interval [props]
+  [:> rn/View {:style {:flex 1 :justify-content "center" :align-items "center"}}
+   [:> rn/Text "interval"]
+   [:> rn/Button {:on-press #(.navigate (:navigation props) "settings")
+                  :title "go to settings"}]])
+
+(defn Settings [props]
+  [:> rn/View {:style {:flex 1 :justify-content "center" :align-items "center"}}
+   [:> rn/Text "settings"]])
 
 (defn navigation []
   [:> NavigationContainer
    [:> (.-Navigator Stack)
     ;; works with var for some reason
-    [:> (.-Screen Stack) {:name "tmp" :component (r/reactify-component #'Home)}]]])
+    [:> (.-Screen Stack) {:name "home" :component (r/reactify-component #'Home)}]
+    [:> (.-Screen Stack) {:name "interval" :component (r/reactify-component #'Interval)}]
+    [:> (.-Screen Stack) {:name "settings" :component (r/reactify-component #'Settings)}]]])
