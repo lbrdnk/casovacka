ef8820a8bea37a3f4ec2646666c0aec6fb381cac
Libor Danek <lbr.dnk@posteo.net>
2023-01-30 18:27:43 +0100

    [WIP] Untrack generated files

--- src/gen/components/Simple.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-import { jsx as _jsx } from "react/jsx-runtime";
-export function Simple() {
-  return /*#__PURE__*/_jsx(View, {
-    children: /*#__PURE__*/_jsx(Text, {
-      children: "ahoj"
-    })
-  });
-}
