c49732488bac27801451a7be53eda625efbe168c
Libor Danek <lbr.dnk@posteo.net>
2023-02-01 11:35:06 +0100

    [WIP] Storybook in tabs with icons

--- src/dev/casovacka/storybook.cljs
+++ src/dev/casovacka/storybook.cljs
@@ -3,7 +3,10 @@
             ["@react-navigation/native" :refer [NavigationContainer]]
             [reagent.core :as r]
             ;; tmp
-            [casovacka.navigation :refer [navigation]]))
+            [casovacka.navigation :refer [navigation]]
+
+            ;; tmp
+            ["@expo/vector-icons/MaterialCommunityIcons$default" :as MaterialCommunityIcons]))
 
 (def Storybook (-> (js/require "../.storybook/Storybook.js") .-default))
 
@@ -12,11 +15,24 @@
 #_(defn storybook [props]
     [:> Storybook])
 
-
+(defn screen-options [^js x]
+  (let [route-name (-> x .-route .-name)]
+    (clj->js {:headerShown false
+              :tabBarIcon (fn [^js y]
+                                (let [icon-name (case route-name
+                                                  "App" "application-outline"
+                                                  "Storybook" "application-edit-outline"
+                                                  "cicina")
+                                      icon-color (.-color y)
+                                      icon-size (.-size y)]
+                                  (r/as-element [:> MaterialCommunityIcons
+                                                 {:name icon-name :size icon-size :color icon-color}])))
+              :tabBarActiveTintColor "tomato"
+              :tabBarInactiveTintColor "gray"})))
 
 (defn with-storybook [props]
   [:> NavigationContainer
-   [:> (.-Navigator Tab) {:screenOptions {:headerShown false}}
+   [:> (.-Navigator Tab) {:screenOptions screen-options}
     [:> (.-Screen Tab)
      {:name "App"
       :component (r/reactify-component
