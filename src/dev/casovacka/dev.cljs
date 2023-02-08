7e5c50941c8d7a7d5e7e66c2483ff00672e69a9f
Libor Danek <lbr.dnk@posteo.net>
2023-02-08 14:15:55 +0100

    Just trying out walk

--- src/dev/casovacka/dev.cljs
+++ src/dev/casovacka/dev.cljs
@@ -1,5 +1,6 @@
 (ns casovacka.dev
-  #_(:require [shadow.cljs.devtools.api :as shadow]))
+  #_(:require [shadow.cljs.devtools.api :as shadow])
+  (:require [clojure.walk :as walk]))
 
 (defonce navigation-state (atom nil))
 
@@ -8,3 +9,41 @@
   shadow/with-runtime
   shadow/watch-set-autobuild!
   )
+
+(comment
+  casovacka.db/empty-db
+  )
+
+;; db initialization should fill in indices used
+(comment
+  ;; unable to walk on, postwalk ok
+  (walk/walk
+   identity
+   #_(fn [elm]
+       (if (sequential? elm)
+         (reduce (fn [acc e-in]
+                   (let [id (:id e-in)]
+                     (assoc acc id e-in)))
+                 {}
+                 elm)
+         elm))
+   (fn [elm]
+     (if (map? elm)
+       (assoc elm :id (str (random-uuid)))
+       elm))
+   #_(fn [elm]
+       (if (sequential? elm)
+         (reduce (fn [acc e-in]
+                   (let [id (:id e-in)]
+                     (assoc acc id e-in)))
+                 {}
+                 elm)
+         elm))
+   casovacka.db/empty-db)
+
+  (walk/postwalk
+   (fn [elm]
+     (if (map? elm)
+       (assoc elm :id (str (random-uuid)))
+       elm))
+   casovacka.db/empty-db))
